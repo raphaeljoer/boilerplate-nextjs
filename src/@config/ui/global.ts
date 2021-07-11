41306d63b067233f5ad27cbaf7f2578e597c02a7
raphaeljoer <ra.joer@gmail.com>
2021-07-11 11:03:43 -0300

    Updated PWA config

--- src/@config/ui/global.ts
+++ src/@config/ui/global.ts
@@ -4,8 +4,4 @@ export const favicon =
   process.env.BOILERPLATE_CONFIG_UI_GLOBAL_FAVICON || `${path}favicon.ico`;
 
 export const openGraph =
-  process.env.BOILERPLATE_CONFIG_UI_GLOBAL_OPENGRAPH || `${path}opengraph.webp`;
-
-export const fonts =
-  process.env.BOILERPLATE_CONFIG_UI_GLOBAL_FONTS ||
-  'https://fonts.googleapis.com/css2?family=Odibee+Sans&family=Roboto:wght@400;500;700&display=swap';
+  process.env.BOILERPLATE_CONFIG_OPENGRAPH_IMAGE_URL || `${path}opengraph.webp`;
